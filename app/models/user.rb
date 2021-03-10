139e4c31d7b262a1eb9dea490995ae498094e894
ChelateLe <kouki0921mk@gmail.com>
2021-03-10 15:48:56 +0900

    モデルの表記修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true, format: /\A[ぁ-んァ-ン一-龥]/
-  validates :first_name_kana, presence: true, format: /\A[ァ-ヶー－]+\z/
-  validates :family_name, presence: true, format: /\A[ぁ-んァ-ン一-龥]/
-  validates :family_name_kana, presence: true, format: /\A[ァ-ヶー－]+\z/
-  validates :birth_date, presence: true 
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: /\A[ぁ-んァ-ン一-龥]/
+    validates :first_name_kana, format: /\A[ァ-ヶー－]+\z/
+    validates :family_name, format: /\A[ぁ-んァ-ン一-龥]/
+    validates :family_name_kana, format: /\A[ァ-ヶー－]+\z/
+    validates :birth_date 
+  end
 
   has_many :items
   has_many :purchase_records
