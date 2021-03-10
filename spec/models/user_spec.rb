94ad943b8b76b3e8917a5cde78bd052abf8055f9
ChelateLe <kouki0921mk@gmail.com>
2021-03-10 14:52:28 +0900

    テストコードがうまく機能するかのテスト

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "ユーザー新規登録" do
-    it "nicknameが空だと登録出来ない"
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it "入力項目を全て正しく入力すれば登録できること" do
+      expect(@user).to be_valid
+    end
+  end  
 end
