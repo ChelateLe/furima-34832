5120b379b41ab4bd1f3167f1fe35d78d70459520
ChelateLe <kouki0921mk@gmail.com>
2021-03-10 12:08:03 +0900

    ユーザー登録機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :first_name, presence: true
-  validates :first_name, presence: true
+  validates :first_name_kana, presence: true
   validates :family_name, presence: true
-  validates :family_name, presence: true
+  validates :family_name_kana, presence: true
   validates :birth_date, presence: true 
 
   has_many :items
