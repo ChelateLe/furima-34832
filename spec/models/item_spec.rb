b09ca3543741b61e1d95ff95b223d8c191f669d3
ChelateLe <kouki0921mk@gmail.com>
2021-03-11 15:40:29 +0900

    テストコード途中まで

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    describe '商品出品機能' do
+      context '出品がうまくいくとき' do
+        it '入力項目を全て満たせば出品できる' do
+          expect(@item).to be_valid
+        end
+      end
+
+      context '出品がうまくいかない時' do
+        it 'nameが空では出品できない' do
+          @item.name = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Name can't be blank")
+        end
+
+        it 'infoが空では出品できない' do
+          @item.info = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Info can't be blank")
+        end
+
+        it 'imageが空では出品できない' do
+          @item.image = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Image can't be blank")
+        end
+      end    
+    end
+  end  
+
 end
