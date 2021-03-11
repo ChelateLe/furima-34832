333d7507f70610d638dccd27e74db0af03907120
ChelateLe <kouki0921mk@gmail.com>
2021-03-11 15:24:59 +0900

    テストコード用準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    name {Faker::Commerce.name}
+    info {Faker::Lorem.sentence}
+    user_id {1}
+    category_id {2}
+    status_id {2}
+    cost_id {2}
+    area_id {2}
+    furimadate_id {2}
+    price {300}
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
