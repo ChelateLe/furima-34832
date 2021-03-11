c4f12f6112f1971a36f4eb69b081f536a43fdfef
ChelateLe <kouki0921mk@gmail.com>
2021-03-11 12:13:39 +0900

    価格の設定（半角のみ＆価格基準）

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,8 @@ class Item < ApplicationRecord
   belongs_to :furimadate
   has_one_attached :image
 
-  validates :name, :info, :price, presence: true
+  validates :name, :info, presence: true
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
   validates :category_id, :status_id, :cost_id, :area_id, :furimadate_id, numericality: { other_than: 1 } 
 end
