3ddd0a1b4b628a7fb5f116fdcf6a414b830669bc
ChelateLe <kouki0921mk@gmail.com>
2021-03-12 09:15:59 +0900

    updateアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
 
   def index
     @items = Item.all.order(created_at: :desc)
