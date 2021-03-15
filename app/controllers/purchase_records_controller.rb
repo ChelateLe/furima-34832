a27db504f1c29836161d4df1cce0f99fa7721551
ChelateLe <kouki0921mk@gmail.com>
2021-03-15 12:46:13 +0900

    とりあえず仮のコントローラー設定

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -2,4 +2,26 @@ class PurchaseRecordsController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
   end
+
+  def new
+    @purchase_address = PurchaseAddress.new
+  end
+
+  def create
+    @purchase_address = PurchaseAddress.new(purchase_record_params)
+    if @purchase_address.valid?
+      @purchase_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def purchase_record_params
+    params.require(:purchase_address).permit(:postal_code, :area_id, :cities, :house_number, :buiding, :phone_number).merge(user_id: current_user.id)
+  end
+
 end
+
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,4 +1,9 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :
+  attr_accessor :postal_code, :area_id, :cities, :house_number, :building, :phone_number, :user_id, :item_id
+
+  def save
+    purchase_record = PurchaseRecord.create(user_id: user_id, item_id: item_id)
+    Address.create(postal_code: postal_code, area_id: area_id, cities: cities, house_number: house_number, building: building, purchase_record_id: purchase_record)
+  end
 end
