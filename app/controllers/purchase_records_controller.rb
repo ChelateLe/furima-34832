176c612099b854e118f11ff5bb000a74475414d3
ChelateLe <kouki0921mk@gmail.com>
2021-03-15 14:22:28 +0900

    仮ストロングパラメーターとルーティング

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -13,14 +13,14 @@ class PurchaseRecordsController < ApplicationController
       @purchase_address.save
       redirect_to root_path
     else
-      render :new
+      render 'index'
     end
   end
 
   private
 
   def purchase_record_params
-    params.require(:purchase_address).permit(:postal_code, :area_id, :cities, :house_number, :buiding, :phone_number).merge(user_id: current_user.id)
+    params.require(:purchase_address).permit(:postal_code, :area_id, :cities, :house_number, :buiding, :phone_number).merge(user_id: current_user.id, item_id: price)
   end
 
 end
