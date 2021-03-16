867ab822e6ed10f10633ee2cb23efee8031bfea6
ChelateLe <kouki0921mk@gmail.com>
2021-03-16 15:04:28 +0900

    環境変数設定クレジット決済途中まで

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,4 +1,14 @@
 class PurchaseRecordsController < ApplicationController
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SELECT_KEY"]
+    Payjp::Charge.create(
+      amount: purchase_record_params[:price],
+      card: purchase_record_params[:token],
+      currency:'jpy'
+    )
+  end
+
   def index
     @item = Item.find(params[:item_id])
   end
@@ -20,7 +30,7 @@ class PurchaseRecordsController < ApplicationController
   private
 
   def purchase_record_params
-    params.require(:purchase_address).permit(:postal_code, :area_id, :cities, :house_number, :buiding, :phone_number).merge(user_id: current_user.id, item_id: price)
+    params.require(:purchase_address).permit(:postal_code, :area_id, :cities, :house_number, :buiding, :phone_number).merge(user_id: current_user.id, item_id: price, token: params[:token])
   end
 
 end
