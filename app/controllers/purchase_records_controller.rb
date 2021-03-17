0707ae9b7fccf84d028e48e1bb062aab33e22379
ChelateLe <kouki0921mk@gmail.com>
2021-03-17 12:09:55 +0900

    before_actionの定義

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,11 +1,13 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :itemsuite
+  before_action :redirectroot, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_record_params)
     if @purchase_address.valid?
       pay_item
@@ -18,6 +20,10 @@ class PurchaseRecordsController < ApplicationController
 
   private
 
+  def itemsuite
+    @item = Item.find(params[:item_id])
+  end
+
   def purchase_record_params
     params.require(:purchase_address).permit(
       :postal_code, :area_id, :cities, :house_number, :building, :phone_number
@@ -25,7 +31,6 @@ class PurchaseRecordsController < ApplicationController
   end
 
   def pay_item
-    @item = Item.find(params[:item_id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
@@ -34,6 +39,11 @@ class PurchaseRecordsController < ApplicationController
     )
   end
 
+  def redirectroot
+    if current_user.id == @item.user_id || @item.purchase_record.present?
+      redirect_to root_path
+    end
+  end 
 
 end
 
