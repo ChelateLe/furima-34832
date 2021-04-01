45822a91a48a9276288500d1e746fff2d8c64c57
ChelateLe <kouki0921mk@gmail.com>
2021-04-01 15:47:55 +0900

    unicornの記述

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,55 @@
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.16.0'
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
+app_path = File.expand_path('../../../', __FILE__)
 
-# Capistranoのログの表示に利用する
-set :application, 'furima-34832'
+#アプリケーションサーバの性能を決定する
+worker_processes 1
 
-# どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:ChelateLe/furima-34832.git'
+#アプリケーションの設置されているディレクトリを指定
+working_directory "#{app_path}/current"
 
-# バージョンが変わっても共通で参照するディレクトリを指定
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+#Unicornの起動に必要なファイルの設置場所を指定
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
-set :rbenv_type, :user
-set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
+#ポート番号を指定
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
-# どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/pear2741.pem'] 
+#エラーのログを記録するファイルを指定
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
-# プロセス番号を記載したファイルの場所
-set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+#通常のログを記録するファイルを指定
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-# Unicornの設定ファイルの場所
-set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
-set :keep_releases, 5
+#Railsアプリケーションの応答を待つ上限時間を設定
+timeout 60
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
+#以下は応用的な設定なので説明は割愛
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+run_once = true
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) &&
+    ActiveRecord::Base.connection.disconnect!
+
+  if run_once
+    run_once = false # prevent from firing again
+  end
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exist?(old_pid) && server.pid != old_pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH => e
+      logger.error e
+    end
   end
 end
+
+after_fork do |_server, _worker|
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
+end鸀
